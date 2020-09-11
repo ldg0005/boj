7f49c4cfe6918d2fb0b4f9c73e521e5153cb2f1a
ldg0005 <ldg0005@gmail.com>
2020-09-11 20:51:00 +0900

    Update 14786(소수점).cpp

--- 14786(소수점).cpp
+++ 14786(소수점).cpp
@@ -1,3 +1,53 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	cout << fixed;
+	cout.precision(15);
+
+	int A, B, C;
+	cin >> A >> B >> C;
+	double x = 0;
+	while(1)
+	{
+		if((A * x) + (B * (sin(x))) >= C) break;
+		x++;
+	}
+	x--;
+	double left = 0, right = 1;
+	double mid;
+	while(left <= right)
+	{
+		mid = (left + right) / 2;
+		double tmp = (A * (x + mid)) + (B * (sin(x + mid)));
+		if(tmp == C) break;
+		else if(tmp > C) right = mid - 0.0000000000000001;
+		else left = mid + 0.0000000000000001;
+	}
+	for(int i = 15; i >= 6; i--)
+		mid = round(mid * pow(10, i)) / pow(10, i);
+	double ans = x + mid;
+	cout << setprecision(6) << ans << endl;
+
+	return 0;
+}
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cmath>
@@ -35,3 +85,5 @@ int main()
 
 	return 0;
 }
+
+
